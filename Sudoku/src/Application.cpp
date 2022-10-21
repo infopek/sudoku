fbd1f34972dbdf8499edd31e9a77c1da5e687cd5
infopek <10saero01@gmail.com>
2022-10-21 17:57:36 +0200

    Clean up Application so that it only instantiates Game

--- Sudoku/src/Application.cpp
+++ Sudoku/src/Application.cpp
@@ -1,50 +1,7 @@
-#include <iostream>
-
-#include <SFML/Graphics.hpp>
-
-#include "Board.h"
-
-#define WIN_WIDTH 1280
-#define WIN_HEIGHT 720
+#include "Game.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "Sudoku", sf::Style::Close);
-
-    sf::Texture texture;
-    texture.loadFromFile("background_gray.jpg");
-    sf::Sprite background(texture);
-    background.setColor(sf::Color(170, 170, 170));
-
-    const sf::Vector2f cellSize(50.0f, 50.0f);
-    const int n = 9;
-    const unsigned int fps = 60;
-
-    Board board(n, sf::Vector2f(50.0f, 50.0f));
-    window.setFramerateLimit(fps);
-    while (window.isOpen())
-    {
-        // Event handling
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            switch (event.type)
-            {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            default:
-                break;
-            }
-        }
-
-        window.clear(sf::Color(42, 42, 42, 1)); // dark grey
-
-        // Draw calls
-        window.draw(background);
-        board.Draw(window);
-
-        window.display();
-    }
-
+    Game sudoku(1280, 720);
+    sudoku.Run();
 }
